b7c6bc56d19097723eeaf642c5e54483e62fa3de
Matheus Amancio Seixeiro <matheusamancio23@hotmail.com>
2015-03-30 09:56:59 -0300

    criado a tableview do calendário

---
+++ EngineerChallenge/VacinasCalendarioTableView.h
@@ -0,0 +1,15 @@
+//
+//  VacinasCalendarioTableView.h
+//  EngineerChallenge
+//
+//  Created by Matheus Amancio Seixeiro on 3/30/15.
+//  Copyright (c) 2015 Isaías Lima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VacinasCalendarioTableView : UITableViewController
+
+@property (weak, nonatomic) IBOutlet UIToolbar *toolEvent;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *expotar;
+@end
